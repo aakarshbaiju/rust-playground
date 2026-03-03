57c6f48853f59b8f9e2c0e9264cc241057efb9f3
Aakarsh <29917454+aakarshbaiju@users.noreply.github.com>
2026-03-03 03:45:55 +0000

    struct method impl

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -9,8 +9,28 @@ struct User {
 // Tuple like structs
 struct Coordinates(i32, i32, i32);
 
-// Unit structs
+// Unit structs: mostly used alongside traits
 struct UnitStruct;
+
+struct Square {
+    width: u32,
+    height: u32,
+}
+
+impl Square {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn get_width(&self) -> u32 {
+        self.width
+    }
+
+    fn change_width(&mut self, new_width: u32) {
+        // self is a reference so it cannot be written to if not marked mutable
+        self.width = new_width;
+    }
+}
 
 fn main() {
     let user1 = User{active: true, username: String::from("John"), sign_in_count: 0};
@@ -25,6 +45,16 @@ fn main() {
     println!("{} {} {}", coords.0, coords.1, coords.2);
 
     // 1..5, .. Range{start:1, end: 5}
+
+
+    // variable should also be mutable for change_width to work
+    let mut sq = Square {width: 5, height: 5};
+
+    println!("{}", sq.area());
+    println!("{}", sq.get_width());
+    sq.change_width(10);
+    println!("{}", sq.area());
+    println!("{}", sq.get_width());
 }
 
 fn build_user(username: String) -> User {
